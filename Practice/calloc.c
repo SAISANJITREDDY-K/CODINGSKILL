2a46c04f0c0d63be29c93f252a6a7b62dcf32db2
K SAI SANJIT REDDY <saisanjitk@gmail.com>
2025-09-22 22:02:28 +0530

    Create calloc.c

---
+++ Practice/calloc.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int *a = (int *)calloc(5, sizeof(int));
+    if (a == NULL) {
+        printf("Memory allocation failed\n");
+        return 1;
+    }
+
+    for (int i = 0; i < 5; i++) {
+        printf("Enter elements: ");
+        scanf("%d", &a[i]);
+    }
+
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", a[i]);
+    }
+
+    free(a);
+    return 0;
+}
